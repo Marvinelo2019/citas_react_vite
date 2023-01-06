84512a24e51d1d42a73e6ec810d2cfc52dcea084
Marvinelo <amdmarvin.lopez@gmail.com>
2023-01-05 21:48:28 -0600

    No fue mucho trabajo pero fue un trabajo honesto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,9 @@ function Header(){
     
     return(
         
-        <h1 className="font-black text-5xl text-center mx-auto md:w-2/3" >Seguimiento Paciente <span className="text-orange-700" >Veterinaria</span>
+        <h1 className="font-black text-5xl text-center mx-auto md:w-2/3" >
+            Seguimiento Pacientes {""} Con GitHub {''} 
+            <span className="text-orange-700" >Veterinaria</span>
         </h1>
     )
 }
